7ee9e549c61cfd0e3b67c606a9bda1c5da7bd594
Juan Buitrago <juanbuitrago2002@outlook.com>
2022-12-09 06:36:32 -0500

    responsive desing

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -37,6 +37,35 @@ function HomePage(data) {
             : "Ordenar alfabéticamente"}
         </button>
       </div>{" "}
+      <ul className=" flex flex-col items-center mt-20 w-full">
+        {people.map((person) => (
+          <li
+            key={person.id}
+            className="px-6 py-2 cursor-pointer bg-white border border-black rounded-lg m-2 box-shadow-card w-2/3 g:w-1/2 xl:w-2/3 flex items-center hover:bg-slate-200
+      "
+          >
+            <Link href="/profile/[id]" as={`/profile/${person.id}`}>
+              <div className="flex">
+                <Image
+                  src={person.picture}
+                  alt={person.fullName}
+                  width={90}
+                  height={90}
+                  objectFit="contain"
+                />
+                <div className="text-sm ml-5 text-gray-800 itlic">
+                  <h1 className="font-bold text-2xl mb-1 text-black not-italic">
+                    {person.fullName}
+                  </h1>
+                  <p>Edad: {person.age}</p>
+                  <p>Género: {person.gender}</p>
+                  <p>Ocupación: {person.occupation}</p>
+                </div>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
